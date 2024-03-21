70c60e35e6f579d0809761f070663e536b765b62
Joel Kasisi <joelkasisi10@gmail.com>
2024-03-21 23:31:36 +0300

    svg fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0, // Disable inlining of assets
+    rollupOptions: {
+      output: {
+        // Ensure that file names are preserved, including those of images
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
 })
