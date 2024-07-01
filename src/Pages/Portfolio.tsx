6b7622003181c05a0e234ebcd18ef6a51c76c2a6
Joel Kasisi <joelkasisi10@gmail.com>
2024-07-01 12:49:23 +0300

    fix

--- src/Pages/Portfolio.tsx
+++ src/Pages/Portfolio.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router";
 import "./portfolio.scss";
-import 'react-device-frameset/styles/marvel-devices.min.css'
 import Section from "../components/PortfolioSection/Section";
 import SectionBtn from "../components/PortfolioSection/SectionBtn";
 import Footer from "../Sections/Footer/Footer";
