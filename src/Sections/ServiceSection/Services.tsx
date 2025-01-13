00885eb475dd2db4fd64522128357cd21e6041cd
Joel Kasisi <joelkasisi10@gmail.com>
2025-01-13 22:01:55 +0200

    redesigned skills section

--- src/Sections/ServiceSection/Services.tsx
+++ src/Sections/ServiceSection/Services.tsx
@@ -1,97 +1,186 @@
-import NodeJS from "../../assets/node-js-2.png"
-import GitLogo from "../../assets/icons8-git-48.png"
+import NodeJS from "../../assets/node-js-2.png";
+import Next from "../../assets/next.svg";
 import { useTheme } from "../../contexts/ThemeContext";
 
 const Services = () => {
   const { isDarkMode } = useTheme();
-    return (
-        <section id="services" className={isDarkMode ? 'dark-mode' : 'light-mode'}>
-        <div className="services container">
-          <div className="service-top">
-            <h1 className="section-title hidden right">
-              Ski<span>ll</span>s
-            </h1>
+  return (
+    <section id="services" className={isDarkMode ? "dark-mode" : "light-mode"}>
+      <div className="services container">
+        <div className="service-top">
+          <h1 className="section-title hidden right">
+            Ski<span>ll</span>s
+          </h1>
+        </div>
+        <div className="service-bottom ">
+          <div className="service-item special-need logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/color/344/html-5--v1.png"
+                alt="HTML"
+              />
+            </div>
+            <p>HyperText Markup Language HTML</p>
           </div>
-          <div className="service-bottom ">
-            <div className="service-item special-need logo hidden">
-              <div className="icon">
-                <img src="https://img.icons8.com/color/344/html-5--v1.png" alt="HTML" />
-              </div>
-              <p>
-                The HyperText Markup Language or HTML is the standard markup
-                language for documents designed to be displayed in a web
-                browser.
-              </p>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img src="https://img.icons8.com/color/344/css3.png" alt="CSS" />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src="https://img.icons8.com/color/344/css3.png" alt="CSS" />
-              </div>
-              <p>
-                Cascading Style Sheets (CSS) is a style sheet language used for
-                describing the presentation of a document written in a markup
-                language.
-              </p>
+            <p>Cascading Style Sheets (CSS)</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/color/344/javascript--v1.png"
+                alt="JavaScript"
+              />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src="https://img.icons8.com/color/344/javascript--v1.png" alt="JavaScript" />
-              </div>
-              <p>
-                JavaScript is a programming language used for web development.It renders in interactive and
-                dynamic fashion.
-              </p>
+            <p>JavaScript</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/fluency/512/typescript--v1.png"
+                alt="TypeScript"
+              />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src="https://img.icons8.com/fluency/512/typescript--v1.png" alt="TypeScript" />
-              </div>
-              <p>
-                TypeScript is a syntactic superset of JavaScript. It means that
-                TypeScript adds syntax on top of JavaScript. It adds a type system.
-              </p>
+            <p>TypeScript</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/office/344/react.png"
+                alt="React"
+              />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src="https://img.icons8.com/office/344/react.png" alt="React" />
-              </div>
-              <p>
-                React is a JavaScript library. Its aim is
-                to allow developers to easily create fast user interfaces for
-                websites and applications alike.
-              </p>
+            <p>React</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src={
+                  isDarkMode
+                    ? "https://img.icons8.com/?size=100&id=N3G7bBnphi53&format=png&color=000000"
+                    : "https://img.icons8.com/?size=100&id=35989&format=png&color=000000"
+                }
+                alt="React Native"
+              />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src="https://img.icons8.com/?size=100&id=35989&format=png&color=000000" alt="React Native" />
-              </div>
-              <p>
-              React Native is an open-source UI software framework used to develop applications for Android and iOS using the React framework.
-              </p>
+            <p>React Native</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img src={NodeJS} alt="Node.js" />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src={NodeJS} alt="Node.js" />
-              </div>
-              <p>
-                Node.js is a cross-platform, open-source JavaScript runtime
-                environment. Developers use it for server-side scripting.
-              </p>
+            <p>Node.js</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=13441&format=png&color=000000"
+                alt="Python"
+              />
             </div>
-            <div className="service-item logo hidden">
-              <div className="icon">
-                <img src={GitLogo} alt="Git" />
-              </div>
-              <p>
-                Git is a distributed version control system.Its goals include
-                speed, data integrity, and support for distributed, non-linear
-                workflows.
-              </p>
+            <p>Python</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src={
+                  isDarkMode
+                    ? Next
+                    : "https://cdn.worldvectorlogo.com/logos/next-js.svg"
+                }
+                alt="Next.js"
+              />
             </div>
+            <p>Next.js</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=20906&format=png&color=000000"
+                alt="Git"
+              />
+            </div>
+            <p>Git</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=38561&format=png&color=000000"
+                alt="postgreSQL"
+              />
+            </div>
+            <p>postgreSQL</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=rgPSE6nAB766&format=png&color=000000"
+                alt="MySQL"
+              />
+            </div>
+            <p>MySQL</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=bosfpvRzNOG8&format=png&color=000000"
+                alt="MongoDB"
+              />
+            </div>
+            <p>MongoDB</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=62452&format=png&color=000000"
+                alt="fireBase"
+              />
+            </div>
+            <p>fireBase</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=22813&format=png&color=000000"
+                alt="Docker"
+              />
+            </div>
+            <p>Docker</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=jD-fJzVguBmw&format=png&color=000000"
+                alt="Redux"
+              />
+            </div>
+            <p>Redux</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=KRA1PoZgRrca&format=png&color=000000"
+                alt="GraphQL"
+              />
+            </div>
+            <p>GraphQL</p>
+          </div>
+          <div className="service-item logo hidden">
+            <div className="icon">
+              <img
+                src="https://img.icons8.com/?size=100&id=55497&format=png&color=000000"
+                alt="REST API"
+              />
+            </div>
+            <p>REST API</p>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default Services;
+      </div>
+    </section>
+  );
+};
+
+export default Services;
