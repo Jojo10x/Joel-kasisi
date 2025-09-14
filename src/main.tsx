4f27d330291aadf77110492acc198a55a73cd5fe
jojo10x <joelkasisi10@gmail.com>
2025-09-14 22:46:23 +0200

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 import { ThemeProvider } from '../src/contexts/ThemeProvider.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
+    <Analytics />
     <App />
   </ThemeProvider>
   </React.StrictMode>,
