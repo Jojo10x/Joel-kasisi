f95efd4c7718ba92f61f9771a235c039674346b2
jojo10x <joelkasisi10@gmail.com>
2025-09-14 22:51:27 +0200

    feat: add @vercel/speed-insights dependency and integrate into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './App.css'
 import { ThemeProvider } from '../src/contexts/ThemeProvider.tsx'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
     <Analytics />
+    <SpeedInsights />
     <App />
   </ThemeProvider>
   </React.StrictMode>,
